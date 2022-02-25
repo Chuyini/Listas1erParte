56b12222898ae3d15a3cd49deebb0e323f624390
Jesus Manuel Lara <jmlr231201@gmail.com>
2022-02-24 21:23:37 -0600

    actualizacion

---
+++ src/uaslp/objetos/list/Linkedlis/Main.java
@@ -0,0 +1,75 @@
+import uaslp.objetos.list.Linkedlis.LinkedList;
+import uaslp.objetos.list.Linkedlis.LinkedListIterator;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        LinkedList team1 = new LinkedList();
+        LinkedList team2 = new LinkedList();
+        LinkedList team3 = new LinkedList();
+
+        team1.addAtTail("Jesús");
+        team1.addAtTail("Salomón");
+        team1.addAtTail("Yael");
+
+        team2.addAtFront("Cristian");
+        team2.addAtFront("Daniel");
+        team2.addAtFront("Diego");
+
+        team3.addAtFront("Imelda");
+
+        LinkedListIterator iterator;
+
+        iterator = team1.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Jesús
+        // Salomón
+        // Yael
+
+        iterator = team2.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Diego
+        // Daniel
+        // Cristian
+
+        team1.remove(0);
+        team1.addAtFront("Rebeca");
+        System.out.println("Team 1 tiene: " + team1.getSize() + " integrantes"); // debe imprimir "Team 1 tiene 3 integrantes"
+
+        iterator = team1.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+
+        // Debió haber impreso
+        // Rebeca
+        // Salomón
+        // Yael
+
+        team2.remove(2);
+        team2.addAtTail("Rita");
+        System.out.println("Team 2 tiene: " + team2.getSize() + " integrantes"); // debe imprimir "Team 2 tiene 3 integrantes"
+
+        iterator = team2.getIterator();
+
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+    }
+}
